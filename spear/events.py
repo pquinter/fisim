e0eb62b07caaabbd565012d615b855409200decb
Porfirio Quintero Cadena <porfirioqc@gmail.com>
2024-10-09 16:44:57 -0400

    validate action and add year in event

--- spear/events.py
+++ spear/events.py
@@ -3,7 +3,7 @@ Event class for financial planning model
 """
 import inspect
 from dataclasses import dataclass
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List
 
 from .flows import InOrOutPerYear
 
@@ -30,17 +30,25 @@ class Action:
     def __post_init__(self):
         self.validate()
 
+    def _mock_call_action_with_params(self):
+        """Mock calling the action with the provided parameters."""
+        target_action = getattr(self.target, self.action)
+        # Get the signature of the target action
+        sig = inspect.signature(target_action)
+        # Check if the provided parameters match the action;
+        params_to_check = self.params.copy()
+        if "year" in sig.parameters:
+            # year comes from Event, so just mock it here if needed
+            params_to_check["year"] = 2024
+        sig.bind(**params_to_check)
+
     def validate(self):
         """
         Raise an error if the target does not have specified property or method,
         or if the parameters are invalid for the target's method.
         """
         try:
-            target_action = getattr(self.target, self.action)
-            # Get the signature of the target action
-            sig = inspect.signature(target_action)
-            # Check if the provided parameters match the action
-            sig.bind(**self.params)
+            self._mock_call_action_with_params()
         except AttributeError:
             raise ValueError(f"'{self.target.__class__.__name__}' has no action '{self.action}'")
         except TypeError as e:
@@ -65,29 +73,27 @@ class Event:
         The name of the event.
     actions : List[Action]
         The actions to be taken by the event.
-    start_year : int, optional
+    year : int
         The year when the event occurs.
 
     Attributes
     ----------
     name : str
-    start_year : int
+    year : int
     actions : List[Action]
     """
 
     name: str
+    year: int
     actions: List[Action]
-    start_year: Optional[int] = None
 
     def __post_init__(self):
-        self.start_year = self.start_year or self.get_earliest_action_year()
-
-    def get_earliest_action_year(self) -> int:
-        """Get the earliest year from the event's actions."""
-        all_years = [action.params.get("year", float("inf")) for action in self.actions]
-        if min(all_years) == float("inf"):
-            raise ValueError("`start_year` must be provided if no actions specify a year.")
-        return min(all_years)
+        # Add year to all actions
+        for action in self.actions:
+            target_action = getattr(action.target, action.action)
+            # Add year parameter if it method requires it
+            if "year" in inspect.signature(target_action).parameters:
+                action.params["year"] = self.year
 
     def apply(self):
         """Apply the event's actions."""
